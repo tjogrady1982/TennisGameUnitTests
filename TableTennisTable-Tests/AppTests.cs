a16039aef871d01131720b10f977442fc92bda18
Tim O'Grady <Tim.OGrady@anthonynolan.org>
2019-06-18 13:24:34 +0100

    Added save and load tests

--- TableTennisTable-Tests/AppTests.cs
+++ TableTennisTable-Tests/AppTests.cs
@@ -38,8 +38,50 @@ namespace TableTennisTable_Tests
         }
 
         [TestMethod]
-        public void TestWinner()
+        public void TestRecordWin()
         {
+            var league = new League();
+            var renderer = new Mock<ILeagueRenderer>();
+            renderer.Setup(r => r.Render(league)).Returns("Rendered League");
+
+            var app = new App(league, renderer.Object, null);
+
+            app.SendCommand("add player Alice");
+            app.SendCommand("add player Bob");
+            app.SendCommand("record win Alice Bob");
+
+            Assert.AreEqual("Alice", app.SendCommand("winner"));
+        }
+
+        [TestMethod]
+        public void TestSaveFile()
+        {
+            var league = new League();
+            var renderer = new Mock<ILeagueRenderer>();
+            var fileSave = new FileService();
+
+            renderer.Setup(r => r.Render(league)).Returns("Rendered League");
+
+
+            var app = new App(league, renderer.Object, fileSave);
+
+            Assert.AreEqual("Saved test.csv", app.SendCommand("save test.csv"));
+
+        }
+
+        [TestMethod]
+        public void TestLoadFile()
+        {
+            var league = new League();
+            var renderer = new Mock<ILeagueRenderer>();
+            var fileSave = new FileService();
+
+            renderer.Setup(r => r.Render(league)).Returns("Rendered League");
+
+
+            var app = new App(league, renderer.Object, fileSave);
+
+            Assert.AreEqual("Loaded test.csv", app.SendCommand("load test.csv"));
 
         }
     }
